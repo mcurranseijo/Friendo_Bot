7d5501beea1acddf57a7896490ea6bc0152bd034
mcurranseijo <68172072+mcurranseijo@users.noreply.github.com>
2020-11-10 20:17:25 -0600

    Update trivia.py

--- bot/cogs/trivia.py
+++ bot/cogs/trivia.py
@@ -1,281 +1,140 @@
-"""Commands for the events module"""
+"""Commands for the trivia module"""
 import discord
 from discord.ext import commands
 import json
 import urllib.request
 import random
+import os
+import asyncio
+from setting import embed_color
 
-answers = []
 tokenID = ""
 
-amounts = {}
-userAnswers = {}
-
-
-class TriviaCog(commands.Cog):
+class TriviaCog(commands.Cog,name='trivia'):
     """
     Commands for the Trivia Questions
     """
 
     def __init__(self, bot):
         self.bot = bot
+        global scores
+        if not os.path.exists("scores.txt"):
+          file = open('scores.txt',"w+")
+          file.write('{"480055359462178826": 0}')
+          file.close()
+        with open('scores.txt') as json_file:
+            self.scores = json.load(json_file)
+        print(self.scores)
 
-    # This batch of commands grab from the category based on their name
-    @commands.command(
-        name="history",
-        brief="grabs a question from the history category and allows you to answer",
-    )
-    async def history(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 23
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
-
-    @commands.command(
-        name="book",
-        brief="grabs a question from the books category and allows you to answer",
-        aliases=["books", "literature"],
-    )
-    async def book(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 10
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
-
-    @commands.command(
-        name="entertainment",
-        brief="grabs a question from the entertainment categories and allows you to answer",
-    )
-    async def entertainment(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = random.randrange(10, 16)
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
-
-    @commands.command(
-        name="theatre",
-        brief="grabs a question from the theatre category and allows you to answer",
-    )
-    async def theatre(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 13
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
-
-    @commands.command(
-        name="film",
-        brief="grabs a question from the film category and allows you to answer",
-        aliases=["films"],
-    )
-    async def film(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 11
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
 
     @commands.command(
         name="trivia",
         brief="grabs a question from a random category and allows you to answer",
     )
     async def trivia(self, ctx):
-        auth_id = str(ctx.message.author.id)
         quiz_var = random.randrange(9, 32)
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
 
-    @commands.command(
-        name="computer",
-        brief="grabs a question from the computer category and allows you to answer",
-        aliases=["computers", "tech"],
-    )
-    async def computer(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 18
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
+        data = [6]
+        data = url_request(quiz_var)
+        embed = discord.Embed(title="TRIVIA",color=embed_color)
 
-    @commands.command(
-        name="cartoon",
-        brief="grabs a question from the cartoon category and allows you to answer",
-        aliases=["cartoons", "anime"],
-    )
-    async def cartoon(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 32
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
+        embed.add_field(name="Category:", value=data[0], inline=True)
+        embed.add_field(name="Difficulty", value=data[1], inline=True)
+        embed.add_field(name="Question", value=data[2], inline=False)
+        embed.add_field(name="1: " + data[3], value="______", inline=False)
+        embed.add_field(name="2: " + data[4], value="______ ", inline=False)
+        embed.add_field(name="3: " + data[5], value="______ ", inline=False)
+        embed.add_field(name="4: " + data[6], value="______ ", inline=False)
+        embed.set_footer(text="Choose your answers, you have 15 seconds")
+        message = await ctx.send(embed=embed)
+        await message.add_reaction("1️⃣")
+        await message.add_reaction("2️⃣")
+        await message.add_reaction("3️⃣")
+        await message.add_reaction("4️⃣")
+        await asyncio.sleep(15)
+        message = await self.bot.get_channel(ctx.channel.id).fetch_message(message.id)
 
-    @commands.command(
-        name="animal",
-        brief="grabs a question from the animal category and allows you to answer",
-        aliases=["animals"],
-    )
-    async def animal(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 27
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
+        if data[7]==1:
+          entrants = [u for u in await message.reactions[0].users().flatten() if not u.bot]
+        elif data[7]==2:
+          entrants = [u for u in await message.reactions[1].users().flatten() if not u.bot]
+        elif data[7]==3:
+          entrants = [u for u in await message.reactions[2].users().flatten() if not u.bot]
+        elif data[7]==4:
+          entrants = [u for u in await message.reactions[3].users().flatten() if not u.bot]
 
-    @commands.command(
-        name="boardgames",
-        brief="grabs a question from the boardgame category and allows you to answer",
-    )
-    async def boardgame(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 16
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
-
-    @commands.command(
-        name="videgame",
-        brief="grabs a question from the videogame category and allows you to answer",
-        aliases=["videogames"],
-    )
-    async def videogame(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 15
-        embed = generate_embed(auth_id, quiz_var)
-
-        await ctx.send(embed=embed)
-
-    @commands.command(
-        name="tv",
-        brief="grabs a question from the tv category and allows you to answer",
-        aliases=["television"],
-    )
-    async def tv(self, ctx):
-        auth_id = str(ctx.message.author.id)
-        quiz_var = 14
-        embed = generate_embed(auth_id, quiz_var)
-        await ctx.send(embed=embed)
-
-    # listens for user input of an answer
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        auth_id = str(message.author.id)
-        ctx = message.channel
-        user_answer = message.content.lower()
+        if len(entrants) > 0:  
+            names = ""
+            for f in entrants:
+              names = names+(f"{f.name}, ")
+              if f.id in self.scores:
+                self.scores[f.id]+=10
+                save(self.scores)
+              else:
+                self.scores[f.id]=10
+                save(self.scores)
+            with open('scores.txt') as json_file:
+              self.scores = json.load(json_file)
+            message = await ctx.send(embed=discord.Embed(title=f"Congrats {names} got it right",description="use !score to check your score and !trivia stats to check the leaderboard!",color=embed_color))
+        else:
+            message = await ctx.send(embed=discord.Embed(title="No one got the right answer",description=f"The correct answer was {data[7]}, react with ✅ to play again",color=embed_color))
+        await message.add_reaction("✅")
+        def check(reaction, user):
+            return reaction.message.channel == ctx.channel and not user.bot
         try:
-            if userAnswers[auth_id] != 0:
-                if user_answer in ["a", "b", "c", "d"]:
-                    correct = userAnswers[auth_id]
-                    if correct == 1:
-                        correct = "a"
-                    elif correct == 2:
-                        correct = "b"
-                    elif correct == 3:
-                        correct = "c"
-                    elif correct == 4:
-                        correct = "d"
-                    embed = discord.Embed(title="Answer")
+          reaction, user = await self.bot.wait_for('reaction_add', timeout= 15, check=check)
+          if reaction.emoji:
+            await self.trivia(ctx)
+        except asyncio.TimeoutError:
+            await ctx.send(embed=discord.Embed(title="No one wanted to play again",color=embed_color))
+  
 
-                    if user_answer == correct:
-                        values = "You, you got the right answer!"
-                        embed.add_field(name="CORRECT!", value=values, inline=False)
-                    else:
-                        values = "Correct answer was: " + str(correct)
-                        embed.add_field(name="INCORRECT!", value=values, inline=False)
-                    userAnswers[auth_id] = 0
-                    await ctx.send(embed=embed)
-
-        except:
-            "user not in array"
-
-    # This command prints an embed listing the categories avaliable
+    #This command prints your scores
     @commands.command(
-        name="categories", brief="returns an embed listing the trivia categories"
+        name="score",
+        brief="returns your score",
     )
-    async def categories(self, ctx):
-        embed = discord.Embed(title="Trivia Bot Categories")
-        embed.add_field(
-            name="-trivia",
-            value="Gives a question from a random category",
-            inline=False,
+    async def score(self,ctx):
+      auth_id = str(ctx.author.id)
+      embed = discord.Embed(title='Your Score',description=str(self.scores[auth_id])+' points',color=embed_color)
+      await ctx.send(embed=embed)
+
+    @commands.command(
+        name="triviastats",
+        brief="returns bot trivia leaderboard",
+    )
+    async def leaderboard(self,ctx):
+        leaderboard = {}
+        for f in self.scores:
+            user = await self.bot.fetch_user(int(f))
+            if user != "none":
+                leaderboard[user.display_name] = self.scores[f]
+          
+
+        sorted_leaderboard = sorted(
+            leaderboard.items(), key=lambda t: t[1], reverse=True
         )
-        embed.add_field(name="-history", value="Gives a history question", inline=False)
-        embed.add_field(name="-books", value="Gives a book question", inline=False)
-        embed.add_field(name="-film", value="Gives a film question", inline=False)
-        embed.add_field(
-            name="-entertainment",
-            value="Gives a question from the film category",
-            inline=False,
-        )
-        embed.add_field(
-            name="-theatre",
-            value="Gives a question from the theatre category",
-            inline=False,
-        )
-        embed.add_field(
-            name="-animals",
-            value="Gives a question from the animal category",
-            inline=False,
-        )
-        embed.add_field(
-            name="-videogames",
-            value="Gives a question from the video games category",
-            inline=False,
-        )
-        embed.add_field(
-            name="-computers",
-            value="Gives a question from the computer category",
-            inline=False,
-        )
-        embed.add_field(
-            name="-cartoons",
-            value="Gives a question from the cartoons category",
-            inline=False,
-        )
-        embed.add_field(
-            name="-boardgames",
-            value="Gives a question from the boardgames category",
-            inline=False,
-        )
-        embed.add_field(
-            name="-tv",
-            value="Gives a question from the television category",
-            inline=False,
-        )
+      
+        leaders = ""
+        for count,staff in enumerate(sorted_leaderboard,1):
+            leaders += f"**{count}** {staff[0]} with {staff[1]} points\n"
+        embed = discord.Embed(title="Leaderboard",description=leaders,color=embed_color)
         await ctx.send(embed=embed)
-
-
-def generate_embed(auth_id, quiz_var):
-    """
-    Generates an embed to send from the data gotten in url_request,
-    takes in a User ID to set the correct answer to and a quizVar for the category number
-    """
-
-    data = [6]
-    data = url_request(quiz_var)
-    userAnswers[auth_id] = int(data[7])
-
-    embed = discord.Embed(title="TRIVIA")
-
-    embed.add_field(name="Category:", value=data[0], inline=True)
-    embed.add_field(name="Difficulty", value=data[1], inline=True)
-    embed.add_field(name="Question", value=data[2], inline=False)
-    embed.add_field(name="A: " + data[3], value="______", inline=False)
-    embed.add_field(name="B: " + data[4], value="______ ", inline=False)
-    embed.add_field(name="C: " + data[5], value="______ ", inline=False)
-    embed.add_field(name="D: " + data[6], value="______ ", inline=False)
-    embed.set_footer(text="Reply with A,B,C,D to answer")
-    return embed
 
 
 def url_request(value: int):
-    """takes in a value for the category of the question and returns an array with the info for the question"""
+    """Handles getting the json from the URL"""
     global tokenID
     url = (
-        "https://opentdb.com/api.php?amount=1&category="
-        + str(value)
-        + "&type=multiple&token="
-        + tokenID
+        type="&type=multiple&token="
+        f"https://opentdb.com/api.php?amount=1&category={str(value}{type}{tokenid}"
     )
 
     response = urllib.request.urlopen(url)
     data = json.load(response)
     responsecode = data["response_code"]
     if str(responsecode) == "4":
-        url2 = "https://opentdb.com/api_token.php?command=reset&token=" + tokenID
+        url2 = f"https://opentdb.com/api_token.php?command=reset&token={tokenid}"
 
     elif str(responsecode) == "3":
         url2 = "https://opentdb.com/api_token.php?command=request"
@@ -328,6 +187,12 @@ def url_request(value: int):
         data[2] = data[2].replace("&#039;", "'")
 
         return data
+
+
+def save(scores):
+    """Saves the user scores to the json file"""
+    with open('scores.txt', 'w') as outfile:
+        json.dump(scores, outfile)
 
 
 def setup(bot):
